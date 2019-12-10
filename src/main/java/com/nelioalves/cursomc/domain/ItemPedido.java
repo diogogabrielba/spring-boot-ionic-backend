ad83634ea8a1bfda4e5ba5ecf6c4dd3a50584742
Diogo Gabriel Barbosa <diogo.gabriel259@gmail.com>
2019-12-10 18:54:34 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
@@ -31,21 +31,28 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
-	
-	
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
 	public ItemPedidoPK getId() {
 		return id;
 	}
@@ -102,7 +109,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
